653a0d4742c88bd923f15598ef4ae21941ecb3a3
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-03-19 18:08:43 -0600

    added the question text into an array in questions.js

---
+++ Assets/JavaScript/questions.js
@@ -0,0 +1,80 @@
+let questionArr=[{
+    Question: "What does DOM stand for?",
+    choice1: "Domination of Model",
+    choice2: "Document Object Model",
+    choice3: "Document of Main Elements",
+    choice4: "Document Object Manipulation",
+    answer: 1
+},
+    {
+    Question: "What does HTML stand for?",
+    choice1: "Hyperlinks and Text Markup Language",
+    choice2: "Hyper Text Markup Language",
+    choice3: "Hyper Text Making Language",
+    choice4: "Hyper Text Mark Language",
+    answer: 1
+    },
+    {
+    Question: "What is one way to return an element in JavaScript?",
+    choice1: "getElementbyID()",
+    choice2: "queryselector()",
+    choice3: "getElementsByClassName()",
+    choice4: "All the Above",
+    answer: 3
+    },
+    {
+    Question: "What does CSS stand for?",
+    choice1: "Colorful StyleSheet",
+    choice2: "Creative Style Sheet",
+    choice3: "Cascading Style Sheet",
+    choice4: "Computer Style Sheet",
+    answer: 2
+    },
+    {
+    Question: "What is the world's most popular programming language?",
+    choice1: "JavaScript",
+    choice2: "Java",
+    choice3: "Python",
+    choice4: "C++",
+    answer: 0
+    },
+    {
+    Question: "How do you insert a comment in a CSS file?",
+    choice1: "/*This is Comment*/",
+    choice2: "//This Is Comment",
+    choice3: "<!--- This Is Comment --->",
+    choice4: "//This Is Comment//",
+    answer: 2
+    },
+    {
+    Question: "Inside which HTML element do we put the JavaScript?",
+    choice1: "<javascript>",
+    choice2: "<js>",
+    choice3: "<scripting>",
+    choice4: "<script>",
+    answer: 3
+    },
+    {
+    Question: "How do you declare a JavaScript variable?",
+    choice1: "v carName;",
+    choice2: "variable carName;",
+    choice3: "var carName;",
+    choice4: "carName=",
+    answer: 2
+    },
+    {
+    Question: "How do you create a function in JavaScript?",
+    choice1: "function myFunction()",  
+    choice2: "function:myFunction()",
+    choice3: "myFunction()",
+    choice4: "function = myFunction()",
+    answer: 0
+    },
+    {
+    Question: "How does a FOR loop start?",
+    choice1: "for (i <= 5; i++)",
+    choice2: "for i = 1 to 5",
+    choice3: "for (i = 0; i <= 5)",
+    choice4: "for (i = 0; i <= 5; i++)",  
+    answer: 3
+    }];
